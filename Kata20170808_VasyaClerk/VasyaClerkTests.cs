721566cb628cf5b066609d6b348e7eb3b20af8d5
cash <cash@cashwu.com>
2017-08-08 20:29:09 +0800

    input 25 test case

--- Kata20170808_VasyaClerk/VasyaClerkTests.cs
+++ Kata20170808_VasyaClerk/VasyaClerkTests.cs
@@ -7,8 +7,22 @@ namespace Kata20170808_VasyaClerk
     public class VasyaClerkTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_25_should_return_Yes()
         {
+            PeopleInLineTicketShouldBe("YES", new[] { 25 });
+        }
+
+        private static void PeopleInLineTicketShouldBe(string expected, int[] peopleInLine)
+        {
+            Assert.AreEqual(expected, new Line().Tickets(peopleInLine));
+        }
+    }
+
+    public class Line
+    {
+        public string Tickets(int[] peopleInLine)
+        {
+            return "YES";
         }
     }
 }
